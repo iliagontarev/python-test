831f823101fcee92a4fd49dc73de574db8b74b8e
Ilia Gontariew <62137863+iliagontarev@users.noreply.github.com>
2020-03-17 22:17:52 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -13,49 +13,55 @@ In case one tries to create a car instance which does not meet these requirement
 class IllegalCarError(Exception):
     pass
 
+
 class Car(object):
     avg_person_mass = 70
-    
-    def __init__(self, pax_count, car_mass, gear_count):
+
+    def init(self, pax_count, car_mass, gear_count):
         self.pax_count = pax_count
         self.car_mass = car_mass
         self.gear_count = gear_count
-    
+
+    # a getter function
     @property
     def total_mass(self):
         return self.car_mass + (self.avg_person_mass * self.pax_count)
-        
-    @pax_count.setter
-    def pax_count(self, count):
-        if count<1:
-            raise IllegalCarError('Minimum passanger required: 1')
-        elif count>5:
-            raise IllegalCarError('Maximum number of passanger is 5!')
-        else:
-            self.__pax_count = count
-    
+
+    # a getter function
     @property
     def pax_count(self):
         return self.__pax_count
-        
+
+
+    @pax_count.setter
+    def pax_count(self, count):
+        if count < 1:
+            raise IllegalCarError('Minimum passanger required: 1')
+        elif count > 5:
+            raise IllegalCarError('Maximum number of passanger is 5!')
+        else:
+            self.__pax_count = count
+
+    # a getter function
+    @property
+    def car_mass(self):
+        return self.__car_mass
+
     @car_mass.setter
     def car_mass(self, mass):
         if mass > 2000:
             raise IllegalCarError("Car mass exceeds the maximum legal weight, sould be less than 2000.")
         else:
             self.__car_mass = mass
-    
-    @property
-    def car_mass(self):
-        return self.__car_mass
-        
-    @gear_count.setter
-    def gear_count(self, count):
-        self.__gear_count = count
-    
+
+    # a getter function
     @property
     def gear_count(self):
         return self.__gear_count
-            
+
+    @gear_count.setter
+    def gear_count(self, count):
+        self.__gear_count = count
+
+
 c = Car(3, 1600, 5)
-        
